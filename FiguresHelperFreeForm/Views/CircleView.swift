cea0df77bb7bc8a7d92ecd2717613b3b4de868b8
Nathan Smith <nsmith@lcs.on.ca>
2021-12-12 16:53:27 -0500

    caught up to commit two

---
+++ FiguresHelperFreeForm/Views/CircleView.swift
@@ -0,0 +1,20 @@
+//
+//  CircleView.swift
+//  FiguresHelperFreeForm
+//
+//  Created by Nate S on 2021-12-12.
+//
+
+import SwiftUI
+
+struct CircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleView()
+    }
+}
---
+++ FiguresHelperFreeForm/Views/SphereView.swift
@@ -0,0 +1,20 @@
+//
+//  SphereView.swift
+//  FiguresHelperFreeForm
+//
+//  Created by Nate S on 2021-12-12.
+//
+
+import SwiftUI
+
+struct SphereView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SphereView_Previews: PreviewProvider {
+    static var previews: some View {
+        SphereView()
+    }
+}
